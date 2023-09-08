86b0251bcdfd70c88ca2c5c7bc63713d3ecb51ea
John Parkhurst <jparkhurst120@gmail.com>
2023-09-08 00:46:31 -0400

    Props Setup

--- client/src/components/Generator/Generator.tsx
+++ client/src/components/Generator/Generator.tsx
@@ -1,16 +1,20 @@
 import React, { FC } from 'react';
 import './Generator.css'; 
-interface GeneratorProps {}
+interface GeneratorProps {
+   projectName:String;
+   projectDesc:String;
+   projectDiff:String;
+   projectLang:String;
+}
 
 function generateIdea(){
    console.log("Generate");
 }
 
-const Generator: FC<GeneratorProps> = () => (
+const Generator: FC<GeneratorProps> = (projectName:"",projectDesc:"",projectDiff:"",projectLang:"") => (
 <div className='Generator-Page'>
+   <h2></h2>
    <button id="generate-btn" onClick={generateIdea}>Generate</button>
-
-
 </div>
 );
 
