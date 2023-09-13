a898a541943987e7496569249995e789b74a17e7
John Parkhurst <jparkhurst120@gmail.com>
2023-09-13 00:49:40 -0400

    Styling

--- client/src/web/components/Generator/Generator.tsx
+++ client/src/web/components/Generator/Generator.tsx
@@ -1,36 +1,48 @@
-import React, { useEffect } from 'react';
-import PropTypes from 'prop-types';
-import { useGeneratorServices } from '../../../services/generator/useGeneratorServices';
-import './test.css';
-import { styled } from 'styled-components';
-
-const Wrapper = styled.div`
+import React, { useEffect } from "react";
+import PropTypes from "prop-types";
+import { useGeneratorServices } from "../../../services/generator/useGeneratorServices";
+import { styled } from "styled-components";
+const Wrapper = styled.main`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center; /* Vertically center the child element */
+  /* Additional styles for the wrapper if needed */
+  height: 80vh; /* Set a specific height to the wrapper, e.g., full viewport height */
 `;
+const GenBox = styled.div`
+  min-width:40%;
+  min-height:40%;
+  background-color:red;
+`;
+import GenerateButtonComponent from "../../common/GenerateBtn/Generatebtn";
+import { Box } from "@mui/material";
 
-export const Generator = ({data, setData}) => {
-   
+export const Generator = ({ data, setData }) => {
   const updateData = async () => {
-   useGeneratorServices(setData);
- };
+    useGeneratorServices(setData);
+  };
 
- useEffect(() => {
-   //Execute this everytime the data changes
- }, [data])
+  useEffect(() => {
+    //Execute this everytime the data changes
+  }, [data]);
 
-   return(
-      <React.Fragment>
-         <Wrapper>
-         <h2>{data.project_name}</h2>
-         <p id="projectdesc">{data.project_desc}</p>
-         <p id="projectlang">{data.project_lang}</p>
-         <p id="projectdiff">{data.project_diff}</p>
-         <button id="generate-btn" onClick={updateData}>Generate</button>
-         </Wrapper>
-      </React.Fragment>
-   );
+  return (
+    <React.Fragment>
+      <Wrapper>
+         <GenBox>
+            <h2>{data.project_name}</h2>
+            <p id="projectdesc">{data.project_desc}</p>
+            <p id="projectlang">{data.project_lang}</p>
+            <p id="projectdiff">{data.project_diff}</p>
+            <GenerateButtonComponent onClick={updateData} />
+          </GenBox>
+      </Wrapper>
+    </React.Fragment>
+  );
 };
 
 Generator.propTypes = {
-   data: PropTypes.shape({}).isRequired,
-   setData: PropTypes.func.isRequired
+  data: PropTypes.shape({}).isRequired,
+  setData: PropTypes.func.isRequired,
 };
