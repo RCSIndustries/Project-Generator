a06bdf20d768992d244619e3c8a33382f10c1156
John Parkhurst <jparkhurst120@gmail.com>
2023-09-20 00:13:58 -0400

    DEPLOYED!!!

--- run.sh
+++ run.sh
@@ -1,9 +1,8 @@
 docker build -f Dockerfile -t john4064/code-catalyst-server . 
-docker build -f Dockerfile.frontend -t john4064/code-catalyst-client .
+docker build -f Dockerfile.prod -t john4064/code-catalyst-client .
 docker run -p 4000:4000  -d john4064/code-catalyst-server
-docker run -p 80:3000  -d john4064/code-catalyst-client
-
+docker run -p 80:3000 -d john4064/project-gen:frontend
 
 #EXAMPLE DOCKER PUSH
 docker tag be51b01f7fef23242f39eaf7edd2c348b890800a6713d13696e895f49bfedc4d john4064/project-gen:backend
-docker push john4064/project-gen:backend
+docker push john4064/project-gen:backend 
