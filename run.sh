f2068a31e98ca74b048000bd47729866b2bbd6a5
John Parkhurst <jparkhurst120@gmail.com>
2023-09-20 00:18:46 -0400

    40 dockerize application (#45)
    * WIP
    
    * Major Progress. Seperate Dockerfiles & Docker Compose Complete
    
    * Prod Dockerfile
    
    * Production ready
    
    * Update run.sh
    
    * DEPLOYED!!!
    
    * Directions to build and deploy

--- run.sh
+++ run.sh
@@ -1,4 +1,12 @@
-docker build -f Dockerfile -t john4064/code-catalyst-server . 
-docker build -f Dockerfile.frontend -t john4064/code-catalyst-client .
+#Cd into client and server for each build and tag
+docker build -f Dockerfile -t john4064/project-gen:backend . 
+docker build -f Dockerfile.prod -t john4064/project-gen:frontend .                            
+#RUN IMAGE to test
 docker run -p 4000:4000  -d john4064/code-catalyst-server
-docker run -p 80:3000  -d john4064/code-catalyst-client
+docker run -p 80:80 -d john4064/project-gen:frontend
+
+#EXAMPLE DOCKER PUSH
+docker tag be51b01f7fef23242f39eaf7edd2c348b890800a6713d13696e895f49bfedc4d john4064/project-gen:backend
+docker push john4064/project-gen:backend 
+
+#Then Just pull and run with the follow run commands
