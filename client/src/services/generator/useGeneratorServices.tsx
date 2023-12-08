1f541527f5e2e6a31e59872e3977ad909b4dfff5
John Parkhurst <jparkhurst120@gmail.com>
2023-12-07 19:28:05 -0500

    93 add difficulty lang selectors (#97)
    * Selector for language
    
    * Update gpt2.csv
    
    * WIP DOCKERFILES FOR CLIENT & SELECTOR
    
    * Fixing the dockercompose for frontend
    
    build front end then run docker compose
    
    * Delete server/exampledata/gpt2.csv
    
    * Update Generator.tsx
    
    * Find By Language

--- client/src/services/generator/useGeneratorServices.tsx
+++ client/src/services/generator/useGeneratorServices.tsx
@@ -4,7 +4,9 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:4000';
 //Service for the generate buttonX
 
-export const useGeneratorServices = async (setData) => {
+export const useGeneratorServices = async (setData,lang) => {
+
+if(lang ==""){
   await axios(`${BASE_URL}/api/generate/`, {
     method: 'GET',
     headers: {
@@ -19,4 +21,20 @@ export const useGeneratorServices = async (setData) => {
     // Handle any errors that may occur during the request
     console.error('An error occurred:', error);
   });
+}else{
+  await axios(`${BASE_URL}/api/generate/lang/`+lang, {
+    method: 'GET',
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*', // Allow requests from any origin
+        },
+  }).then(response => {
+    // You can access the response data here
+    const responseData = response.data[0];
+    setData(responseData);
+  }).catch(error => {
+    // Handle any errors that may occur during the request
+    console.error('An error occurred:', error);
+  });
+  }
  }
